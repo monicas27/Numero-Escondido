1892a07e83149e5be5bf4442252f2c94d238b220
Monica <monicasilvasouza94@gmail.com>
2025-01-16 23:45:06 -0300

    Descrição das mudanças

---
+++ co.py
@@ -0,0 +1,7 @@
+numero= int(input('Digite um número para adivinhar qual é o número secreto:'))
+if numero <100:
+  print('Você errou, não é o numero escondido')
+  
+else:
+ print('Você acertou! o número escondido era 100')
+  
